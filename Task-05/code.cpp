ce0d98ca28852f14f554c3c5b613515ff5954167
Vaishnavi Bholane <87429198+vaishnavi-bholane@users.noreply.github.com>
2021-10-22 16:13:54 +0530

    Add files via upload

---
+++ Task-05/code.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+
+using namespace std;
+
+int factorial(int n)
+{
+	if(n>1){
+		return n * factorial(n-1);
+	}
+	else{
+		return 1;
+	}
+
+}
+
+int main()
+{
+	int factorial(int);
+	int fact, value;
+	cout << "Enter a number: ";
+	cin >> value;
+	fact = factorial(value);
+	cout << "Factorial of the number is:"<<fact<<endl;
+	return 0;
+
+}
